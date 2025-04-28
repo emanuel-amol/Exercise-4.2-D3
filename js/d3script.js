1c0a6f207b2b9d41ecb1e09b98d004f2feb8b40f
104515032 <104515032@student.swin.edu.au>
2025-04-29 00:48:37 +1000

    Added paragraph dynamically to div using D3

--- js/d3script.js
+++ js/d3script.js
@@ -3,3 +3,8 @@ d3.select("h1")
   .style("color", "green")
   .style("font-family", "Arial")
   .style("font-size", "30px");
+
+// Add a paragraph inside a div
+d3.select("div")
+  .append("p")
+  .text("Purchasing a low energy consumption TV will help with your energy bills!");
